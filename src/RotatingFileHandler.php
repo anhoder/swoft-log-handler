375b368d88b748b1f727b06b2e777d6cb8344f7e
anhoder <anhoder@88.com>
2021-01-06 12:31:54 +0800

    fix

--- src/RotatingFileHandler.php
+++ src/RotatingFileHandler.php
@@ -17,6 +17,12 @@ use Swoft\Log\Handler\FileHandler;
  */
 class RotatingFileHandler extends FileHandler
 {
+    public const INTERVAL_YEAR     = 'year';
+    public const INTERVAL_MONTH    = 'month';
+    public const INTERVAL_DAY      = 'day';
+    public const INTERVAL_HOUR     = 'hour';
+    public const INTERVAL_MINUTE   = 'minute';
+
     /**
      * 0 => unlimited
      * @var int
@@ -24,41 +30,56 @@ class RotatingFileHandler extends FileHandler
     protected $maxFiles = 0;
 
     /**
+     * 检查日志文件数量的时间间隔
+     * @var string
+     */
+    protected $checkLogInterval = self::INTERVAL_DAY;
+
+    /**
      * @var bool|null
      */
-    protected $willRotate;
+    private $willRotate;
+
+    /**
+     * @var int
+     */
+    private $checkTime;
+
+    /**
+     * @param string $interval
+     * @return int
+     */
+    protected static function getCheckTime(string $interval)
+    {
+        switch ($interval) {
+            case self::INTERVAL_YEAR:
+                return date('Y0101000000');
+            case self::INTERVAL_MONTH:
+                return date('Ym01000000');
+            case self::INTERVAL_DAY:
+                return date('Ymd000000');
+            case self::INTERVAL_HOUR:
+                return date('YmdH0000');
+            case self::INTERVAL_MINUTE:
+                return date('YmdHi00');
+            default:
+                throw new \InvalidArgumentException('参数错误');
+        }
+    }
 
     /**
      * @inheritdoc
      */
     protected function write(array $records): void
     {
+        $checkTime = self::getCheckTime($this->checkLogInterval);
         // on the first record written, if the log is new, we should rotate (once per day)
-        if (null === $this->willRotate) {
+        if ($this->checkTime != $checkTime) {
+            $this->checkTime = $checkTime;
             $this->willRotate = !file_exists($this->formatFile($this->logFile));
         }
 
         parent::write($records);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function close()
-    {
-        parent::close();
-
-        if (true === $this->willRotate) {
-            $this->rotate();
-        }
-    }
-
-    /**
-     * reset
-     */
-    public function reset()
-    {
-        parent::reset();
 
         if (true === $this->willRotate) {
             $this->rotate();
